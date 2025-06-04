0bde31c71ba2543d2f4b5f8f3ee9d80de0f38971
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2025-06-04 19:21:41 +0700

    fix: einstall tailwindcss v3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "node:path";
 import { normalizePath } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import { defineConfig } from "vitest/config";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,8 +18,10 @@ export default defineConfig({
 				},
 			],
 		}),
-		tailwindcss(),
 	],
+	css: {
+		postcss: "./postcss.config.js",
+	},
 	server: {
 		host: true,
 		strictPort: true,
